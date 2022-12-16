a2141a45833e507ca2152ffb73138e566351361c
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2022-12-16 07:21:29 -0700

    Add deploy to github pages script

---
+++ scripts/deploy-to-github-pages.sh
@@ -0,0 +1,4 @@
+cd /Users/jadenmounteer/git/dodgeballArcade
+npm i angular-cli-ghpages --save-dev
+ng build --base-href "https://jadenmounteer.github.io/dodgeballArcade/"
+npx angular-cli-ghpages --dir=dist/dodgeball-arcade
