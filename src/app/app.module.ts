8a33b64e7e920f1389cf78e97ce7d8c12f312064
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2022-12-21 12:02:57 -0700

    Remove sprite service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlayerComponent } from './components/player/player.component';
+import { SpriteComponent } from './components/sprite/sprite.component';
 
 @NgModule({
-  declarations: [AppComponent, PlayerComponent],
+  declarations: [AppComponent, PlayerComponent, SpriteComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/player/player.component.ts
+++ src/app/components/player/player.component.ts
@@ -6,48 +6,35 @@ import {
   Input,
   ViewChild,
 } from '@angular/core';
-import { SpriteService } from 'src/app/services/sprite.service';
+import { SpriteComponent } from '../sprite/sprite.component';
 
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
   styleUrls: ['./player.component.scss'],
 })
-export class PlayerComponent implements AfterViewInit {
-  public SPRITE_WIDTH = 500; // The total width in px divided by the number of columns
-  public SPRITE_HEIGHT = 500; // The total height in px divided by the total rows
-  public BORDER_WIDTH: number = 1;
-  public SPACING_WIDTH: number = 1;
-  public canvas: any;
-  public context: any;
+export class PlayerComponent extends SpriteComponent implements AfterViewInit {
+  public override SPRITE_WIDTH = 500; // The total width in px divided by the number of columns
+  public override SPRITE_HEIGHT = 500; // The total height in px divided by the total rows
+  public override BORDER_WIDTH: number = 1;
+  public override SPACING_WIDTH: number = 1;
+  public override canvas: any;
+  public override context: any;
   spriteSheetURL = 'assets/sprites/player/Player.png';
-  public frameIndex: number = 0;
-  public frame: any;
-  public image = new Image();
+  public override frameIndex: number = 0;
+  public override frame: any;
+  public override image = new Image();
 
   @Input() canvasQuery: any;
 
   @ViewChild('sprite', { static: true }) player1: ElementRef | undefined;
 
   // Animations
-  private playerStandingStill: any;
-  standingStillCycle: any;
+  private playerStandingStill: any = this.spritePositionToImagePosition(0, 0);
+  private standingStillCycle: any = [this.playerStandingStill];
 
   // Speeds
   walkingSpeed = 500;
-
-  constructor(private spriteService: SpriteService) {
-    this.playerStandingStill = this.spriteService.spritePositionToImagePosition(
-      0,
-      0,
-      this.BORDER_WIDTH,
-      this.SPACING_WIDTH,
-      this.SPRITE_WIDTH,
-      this.SPRITE_HEIGHT
-    );
-
-    this.standingStillCycle = [this.playerStandingStill];
-  }
 
   ngAfterViewInit(): void {
     this.canvas = this.player1?.nativeElement;
@@ -60,16 +47,7 @@ export class PlayerComponent implements AfterViewInit {
 
   public standStill() {
     setInterval(() => {
-      this.spriteService.animate(
-        this.standingStillCycle,
-        this.frameIndex,
-        this.frame,
-        this.context,
-        this.canvas,
-        this.image,
-        this.SPRITE_WIDTH,
-        this.SPRITE_HEIGHT
-      );
+      this.animate(this.standingStillCycle);
     }, this.walkingSpeed);
   }
 }
