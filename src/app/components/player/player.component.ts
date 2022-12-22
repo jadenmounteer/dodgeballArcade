745ac51cabcd9d923d635af02fa82db3fd4a143c
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2022-12-21 21:44:19 -0700

    Add idle animation and new spritesheet

--- src/app/components/player/player.component.ts
+++ src/app/components/player/player.component.ts
@@ -14,8 +14,8 @@ import { SpriteComponent } from '../sprite/sprite.component';
   styleUrls: ['./player.component.scss'],
 })
 export class PlayerComponent extends SpriteComponent implements AfterViewInit {
-  public override SPRITE_WIDTH = 500; // The total width in px divided by the number of columns
-  public override SPRITE_HEIGHT = 500; // The total height in px divided by the total rows
+  public override SPRITE_WIDTH = 128; // The total width in px divided by the number of columns
+  public override SPRITE_HEIGHT = 128; // The total height in px divided by the total rows
   public override BORDER_WIDTH: number = 1;
   public override SPACING_WIDTH: number = 1;
   public override canvas: any;
@@ -29,9 +29,22 @@ export class PlayerComponent extends SpriteComponent implements AfterViewInit {
 
   @ViewChild('sprite', { static: true }) player1: ElementRef | undefined;
 
-  // Animations
+  /*** Images ***/
   private playerStandingStill: any = this.spritePositionToImagePosition(0, 0);
+  private playerStretching1: any = this.spritePositionToImagePosition(0, 1);
+  private playerStretching2: any = this.spritePositionToImagePosition(0, 2);
+
+  /*** ANIMATIONS ***/
+
+  // Standing still
   private standingStillCycle: any = [this.playerStandingStill];
+
+  // Idle
+  private playerIdleCycle: any = [
+    this.playerStandingStill,
+    this.playerStretching1,
+    this.playerStretching2,
+  ];
 
   // Speeds
   walkingSpeed = 500;
@@ -42,12 +55,19 @@ export class PlayerComponent extends SpriteComponent implements AfterViewInit {
     this.image.src = this.spriteSheetURL;
     this.image.crossOrigin = 'true';
     this.canvasQuery = this.canvasQuery;
-    this.standStill();
+    // this.standStill();
+    this.idle();
   }
 
   public standStill() {
     setInterval(() => {
       this.animate(this.standingStillCycle);
+    }, this.walkingSpeed);
+  }
+
+  public idle() {
+    setInterval(() => {
+      this.animate(this.playerIdleCycle);
     }, this.walkingSpeed);
   }
 }
