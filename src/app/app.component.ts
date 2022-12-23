4c8982c8f443a06d8e4e7145337697514a59b2f9
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2022-12-23 10:06:59 -0700

    Add keyboard events handler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'dodgeball-arcade';
+
+  ngOnInit(): void {
+    this.handleKeyBoardEvents();
+  }
+
+  private handleKeyBoardEvents() {
+    window.addEventListener(
+      'keydown',
+      (e) => {
+        e.preventDefault();
+
+        if (e.key === 'ArrowUp') {
+          console.log('Up');
+        } else if (e.key === 'ArrowDown') {
+          console.log('Down');
+        } else if (e.key === 'ArrowLeft') {
+          console.log('Left');
+        } else if (e.key === 'ArrowRight') {
+          console.log('right');
+        }
+      },
+      {
+        capture: true, // this disables arrow key scrolling in modern Chrome
+        passive: false, // this is optional, my code works without it
+      }
+    );
+  }
 }
--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { HostListener, Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 
 @Injectable({
