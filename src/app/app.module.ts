eca0695f9d3716b34cf83a997aaa7fd4f9a9f919
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2022-12-28 08:49:56 -0700

    Add game component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { PlayerComponent } from './components/player/player.component';
 import { SpriteComponent } from './components/sprite/sprite.component';
 import { PlayerShowcaseComponent } from './components/player-showcase/player-showcase.component';
+import { GameComponent } from './components/game/game.component';
 
 @NgModule({
-  declarations: [AppComponent, PlayerComponent, SpriteComponent, PlayerShowcaseComponent],
+  declarations: [AppComponent, PlayerComponent, SpriteComponent, PlayerShowcaseComponent, GameComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
